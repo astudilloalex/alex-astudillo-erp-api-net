4423a7b454ca71bc0309e874911742a6b0427ba9
Alex Astudillo <astudilloalex1@gmail.com>
2023-05-22 23:02:32 -0500

    Add company service interface

---
+++ AlexAstudilloERP.Domain/Interfaces/Services/Public/ICompanyService.cs
@@ -0,0 +1,8 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+
+namespace AlexAstudilloERP.Domain.Interfaces.Services.Public;
+
+public interface ICompanyService
+{
+    public Task<Company> AddAsync(Company company, string token);
+}
---
+++ AlexAstudilloERP.Infrastructure/Repositories/Public/CompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+using AlexAstudilloERP.Domain.Interfaces.Repositories.Public;
+using AlexAstudilloERP.Infrastructure.Connections;
+
+namespace AlexAstudilloERP.Infrastructure.Repositories.Public;
+
+public class CompanyRepository : NPPostgreSQLRepository<Company, int>, ICompanyRepository
+{
+    public CompanyRepository(PostgreSQLContext context) : base(context)
+    {
+    }
+}
