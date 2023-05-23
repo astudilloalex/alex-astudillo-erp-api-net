8d8187314f0a04ecc7ff0e3c862132fd122a4f27
Alex Astudillo <astudilloalex1@gmail.com>
2023-05-22 23:23:26 -0500

    Add company service interface

---
+++ AlexAstudilloERP.Application/Services/Public/CompanyService.cs
@@ -0,0 +1,20 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+using AlexAstudilloERP.Domain.Interfaces.Repositories.Public;
+using AlexAstudilloERP.Domain.Interfaces.Services.Public;
+
+namespace AlexAstudilloERP.Application.Services.Public;
+
+public class CompanyService : ICompanyService
+{
+    private readonly ICompanyRepository _repository;
+
+    public CompanyService(ICompanyRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<Company> AddAsync(Company company, string token)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AlexAstudilloERP.Domain/Interfaces/Services/Custom/IValidateData.cs
@@ -0,0 +1,10 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+
+namespace AlexAstudilloERP.Domain.Interfaces.Services.Custom;
+
+public interface IValidateData
+{
+    public void ValidateCompany(Company company, bool update = false);
+    public void ValidateEmail(Email email, bool update = false);
+    public void ValidatePerson(Person person, bool update = false);
+}
