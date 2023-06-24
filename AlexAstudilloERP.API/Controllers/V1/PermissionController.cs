2a1496c3cf9c5d04d31e55dddb76e517ebe22783
Alex Astudillo <astudilloalex1@gmail.com>
2023-06-24 00:16:42 -0500

    Add permission controller

---
+++ AlexAstudilloERP.API/Controllers/V1/PermissionController.cs
@@ -0,0 +1,29 @@
+﻿using AlexAstudilloERP.API.Handlers;
+using AlexAstudilloERP.Domain.Interfaces.Services.Public;
+using EFCommonCRUD.Interfaces;
+using EFCommonCRUD.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexAstudilloERP.API.Controllers.V1;
+
+[Route("api/v1/permission")]
+[ApiController]
+[Authorize]
+public class PermissionController : CommonController
+{
+    private readonly IPermissionService _service;
+
+    public PermissionController(IPermissionService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    [Route("all/{companyId}")]
+    public async Task<IActionResult> All(int companyId, [FromQuery] int page = 1, [FromQuery] int size = 10)
+    {
+        IPageable pageable = PageRequest.Of(page - 1, size);
+        return Ok(ResponseHandler.Ok(await _service.GetAll(pageable, companyId, Token)));
+    }
+}
