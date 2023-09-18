8dccc7bf8c9958252de1ddc1b452a60a9bfd046c
Alex Astudillo <astudilloalex1@gmail.com>
2023-09-17 20:12:17 -0500

    Skip token verification attribute

---
+++ AlexAstudilloERP.API/Attributes/SkipTokenValidationAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace AlexAstudilloERP.API.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+public class SkipTokenValidationAttribute : Attribute
+{
+}
