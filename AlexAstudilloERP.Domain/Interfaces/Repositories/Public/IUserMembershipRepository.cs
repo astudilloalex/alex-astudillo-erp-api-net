d872f155bc24c23dad6002545899763dd3637214
Alex Astudillo <astudilloalex1@gmail.com>
2023-09-26 23:32:21 -0500

    feat: Implements UserMembershipRepository

---
+++ AlexAstudilloERP.Domain/Interfaces/Repositories/Public/IUserMembershipRepository.cs
@@ -0,0 +1,8 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+
+namespace AlexAstudilloERP.Domain.Interfaces.Repositories.Public;
+
+public interface IUserMembershipRepository
+{
+    public Task<List<UserMembership>> FindByUserCode(string userCode);
+}
---
+++ AlexAstudilloERP.Infrastructure/Repositories/Public/UserMembershipRepository.cs
@@ -0,0 +1,23 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+using AlexAstudilloERP.Domain.Interfaces.Repositories.Public;
+using AlexAstudilloERP.Infrastructure.Connections;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlexAstudilloERP.Infrastructure.Repositories.Public;
+
+public class UserMembershipRepository : IUserMembershipRepository
+{
+    private readonly PostgreSQLContext _context;
+
+    public UserMembershipRepository(PostgreSQLContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<UserMembership>> FindByUserCode(string userCode)
+    {
+        return _context.UserMemberships.AsNoTracking()
+            .Where(um => um.User.Code.Equals(userCode))
+            .ToListAsync();
+    }
+}
