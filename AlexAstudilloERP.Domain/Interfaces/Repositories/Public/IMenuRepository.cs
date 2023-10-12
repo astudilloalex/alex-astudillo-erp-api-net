696e02d8d0d61eabfc490fabd2f4ff8e70f1e0e7
Alex Astudillo <astudilloalex1@gmail.com>
2023-10-11 22:34:11 -0500

    feat: Add menu repository

---
+++ AlexAstudilloERP.Domain/Interfaces/Repositories/Public/IMenuRepository.cs
@@ -0,0 +1,8 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+
+namespace AlexAstudilloERP.Domain.Interfaces.Repositories.Public;
+
+public interface IMenuRepository
+{
+    public Task<List<Menu>> FindByUserCodeAndCompanyCodeAsync(string userCode, string companyCode);
+}
---
+++ AlexAstudilloERP.Infrastructure/Repositories/Public/MenuRepository.cs
@@ -0,0 +1,35 @@
+﻿using AlexAstudilloERP.Domain.Entities.Public;
+using AlexAstudilloERP.Domain.Interfaces.Repositories.Public;
+using AlexAstudilloERP.Infrastructure.Connections;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlexAstudilloERP.Infrastructure.Repositories.Public;
+
+public class MenuRepository : IMenuRepository
+{
+    private readonly PostgreSQLContext _context;
+
+    public MenuRepository(PostgreSQLContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Menu>> FindByUserCodeAndCompanyCodeAsync(string userCode, string companyCode)
+    {
+        string query = @"SELECT m.*
+FROM menus m
+WHERE EXISTS (
+    SELECT 1
+    FROM permission_menus pm
+    INNER JOIN permissions p ON pm.permission_id = p.id
+    INNER JOIN role_permissions rp ON p.id = rp.permission_id
+    INNER JOIN roles r ON rp.role_id = r.id
+    INNER JOIN user_roles ur ON r.id = ur.role_id
+    INNER JOIN users u ON ur.user_id = u.id
+    INNER JOIN companies c ON r.company_id = c.id
+    WHERE u.code = {0} AND c.code = {1}
+    AND m.id = pm.menu_id
+)";
+        return _context.Menus.FromSqlRaw(query, new object[] { userCode, companyCode }).AsNoTracking().ToListAsync();
+    }
+}
